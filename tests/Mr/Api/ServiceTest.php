2778e397867d1516527dad207da55779a0168846
Armando Pérez Marqués <armando.perez@mobilerider.com>
2013-07-10 16:38:04 -0400

    Added more tests
    - ReadOnlyAdapterTest::testGetDisallowedMethods
    - ClientTest::testExceptionThrownForInvalidAdapterTypes
    - ServiceTest::testGetAllChannelRepositoryObjects_One
    - ServiceTest::testGetAllChannelRepositoryObjects_Two

--- tests/Mr/Api/ServiceTest.php
+++ tests/Mr/Api/ServiceTest.php
@@ -7,6 +7,8 @@ use Mr\Api\Http\Response;
 use Mr\Api\AbstractClient;
 use Mr\Api\Service;
 use Mr\Api\Http\Adapter\MockAdapter;
+use Mr\Api\Model\Channel;
+use Mr\Api\Model\Media;
 
 class ServiceTest extends \PHPUnit_Framework_TestCase {
 
@@ -20,28 +22,47 @@ class ServiceTest extends \PHPUnit_Framework_TestCase {
         $this->_service = new Service(self::APP_ID, self::APP_SECRET);
     }
 
+    protected function _mockTheServiceClient($content, $path='/test/path', $status=Response::STATUS_OK) {
+        $mockAdapter = new MockAdapter();
+        // Adds mock basic OK response
+        $mockAdapter->addResponseBy($status, $path, $content);
+        // Add mock adapter
+        $service_client = $this->_service->getClient();
+        $service_client->setAdapter($mockAdapter);
+    }
+
     public function testCreateChannelRepositoryObject() {
-        $data = array('some' => 'data');
+        // No need to mock adapters here, until we call `$obj->save()`
+        $data = array('id'=> 1, 'name' => 'some_name');
         $obj = $this->_service->create('Channel', $data);
         $this->assertEquals($data, $obj->getData());
     }
 
     public function testCreateMediaRepositoryObject() {
-        $data = array('some' => 'data');
+        // No need to mock adapters here, until we call `$obj->save()`
+        $data = array('id'=> 1, 'name' => 'some_name');
         $obj = $this->_service->create('Media', $data);
         $this->assertEquals($data, $obj->getData());
     }
 
-    public function testGetAllChannelRepositoryObjects() {
-
-        $path = '/test/path';
-        $mockAdapter = new MockAdapter();
-        // Adds mock basic OK response
-        $mockAdapter->addResponseBy(Response::STATUS_OK, $path, '[]');
-        // Add mock adapter
-        $service_client = $this->_service->getClient();
-        $service_client->setAdapter($mockAdapter);
-
+    public function testGetAllChannelRepositoryObjects_Empty() {
+        $this->_mockTheServiceClient('{"status": "ok", "objects": [] }');
         $this->assertCount(0, $this->_service->getAll('Channel'));
+    }
+
+    public function testGetAllChannelRepositoryObjects_One() {
+        $this->_mockTheServiceClient('{"status": "ok", "object": {"id": 1, "name": "some_name"} }');
+        $channel = $this->_service->get('Channel', 1);
+        $this->assertInstanceOf('Mr\Api\Model\Channel', $channel);
+        $this->assertEquals($channel->getId(), 1);
+    }
+
+    public function testGetAllChannelRepositoryObjects_Two() {
+        $this->_mockTheServiceClient('{"status": "ok", "objects": [{"id": 1, "name": "some_name"}, {"id": 2, "name": "some_other_name"} ] }');
+
+        $channels = $this->_service->getAll('Channel');
+        $this->assertCount(2, $channels);
+        $this->assertEquals(1, $channels[0]->getId());
+        $this->assertEquals(2, $channels[1]->getId());
     }
 }
