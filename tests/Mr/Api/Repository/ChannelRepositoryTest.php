a45c16996f4192fba55d17d597c838cf3890d699
Armando Pérez Marqués <armando.perez@mobilerider.com>
2013-07-11 19:40:34 -0400

    Added more tests - 85% code coverage

--- tests/Mr/Api/Repository/ChannelRepositoryTest.php
+++ tests/Mr/Api/Repository/ChannelRepositoryTest.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace MrTest\Api\Repository;
 
@@ -10,7 +10,82 @@ use Mr\Api\Http\Adapter\MockAdapter;
 use Mr\Api\Model\Channel;
 use Mr\Api\Repository\ChannelRepository;
 
-class ApiRepositoryTest extends \PHPUnit_Framework_TestCase
+
+/*
+ *  Channel subclass to test if hook methods are called
+ */
+class SubclassedChannel extends Channel {
+    protected $_before_save_called = false;
+    protected $_after_save_called = false;
+    protected $_before_delete_called = false;
+    protected $_after_delete_called = false;
+
+    protected function resetTestState() {
+        $this->_before_save_called = false;
+        $this->_after_save_called = false;
+        $this->_before_delete_called = false;
+        $this->_after_delete_called = false;
+    }
+
+    public function wasBeforeSaveCalled() {
+        return $this->_before_save_called;
+    }
+
+    public function wasAfterSaveCalled() {
+        return $this->_after_save_called;
+    }
+
+    public function wasBeforeDeleteCalled() {
+        return $this->_before_delete_called;
+    }
+
+    public function wasAfterDeleteCalled() {
+        return $this->_after_delete_called;
+    }
+
+    public function beforeSave() {
+        $this->_before_save_called = true;
+        return parent::beforeSave();
+    }
+
+    public function afterSave() {
+        $this->_after_save_called = true;
+        return parent::afterSave();
+    }
+
+    public function beforeDelete() {
+        $this->_before_delete_called = true;
+        return parent::beforeDelete();
+    }
+
+    public function afterDelete() {
+        $this->_after_delete_called = true;
+        return parent::afterDelete();
+    }
+}
+
+class SubclassedChannelRepository extends ChannelRepository {
+
+    /**
+    * Returns current model name, eg: Media
+    *
+    * @return string
+    */
+    public function getModel()
+    {
+        // preg_match("/([^\\\]+$)/", get_called_class(), $matches);
+        // return str_replace('Repository', '', $matches[0]);
+        return parent::getModel();
+    }
+
+    public function create($data = null)
+    {
+        // $modelClass = self::MODEL_NAMESPACE . $this->getModel();
+        return new SubclassedChannel($this, $data);
+    }
+}
+
+class ChannelRepositoryTest extends \PHPUnit_Framework_TestCase
 {
     const HOST = 'http://test.host.com';
     const USERNAME = 'user';
@@ -52,7 +127,7 @@ class ApiRepositoryTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->_client = new Client(self::HOST, self::USERNAME, self::PASSWORD);
-        $this->_clientMockAdapter = new MockAdapter(); 
+        $this->_clientMockAdapter = new MockAdapter();
         $this->_client->setAdapter($this->_clientMockAdapter);
     }
 
@@ -107,12 +182,99 @@ class ApiRepositoryTest extends \PHPUnit_Framework_TestCase
         // Check object returned is not null
         \PHPUnit_Framework_Assert::assertNotNull($channel);
 
-        if ($channel) {
-            // Check return item type
-            \PHPUnit_Framework_Assert::assertInstanceOf(self::MODEL_NAMESPACE . 'Channel', $channel);
-        }
+        // Check return item type
+        \PHPUnit_Framework_Assert::assertInstanceOf(self::MODEL_NAMESPACE . 'Channel', $channel);
 
         // Validating fields data
         $this->validatesObjectData(array($this->channelData['object']), $channel);
+    }
+
+    // TODO: This should be a dedicated exception for when an object is not found,
+    //       like "Mr\Exception\EntityNotFoundException"
+    /**
+     * @expectedException Mr\Exception\ServerErrorException
+     */
+    public function testGetOneChannelNotFound() {
+        // Creating response with specific url
+        $this->_clientMockAdapter->addResponseBy(
+            Response::STATUS_OK,
+            'api/channel/1',
+            json_encode(array('status' => 'Unknown Channel')));
+
+        $repo = new ChannelRepository($this->_client);
+
+        $this->assertNull($repo->get($this->channelData['object']['id']));
+    }
+
+    /**
+     * @expectedException Mr\Exception\DeniedEntityAccessException
+     */
+    public function testGetOneChannelAccessDenied()
+    {
+        // Creating response with specific url
+        $this->_clientMockAdapter->addResponseBy(
+            Response::STATUS_OK,
+            'api/channel/1',
+            json_encode(array('status' => 'Access denied')));
+
+        $repo = new ChannelRepository($this->_client);
+
+        $this->assertNull($repo->get($this->channelData['object']['id']));
+    }
+
+    public function testDeleteOneChannelAndBeforeAfterHooksAreCalled() {
+        // Creating response with specific url
+        $this->_clientMockAdapter->addResponseBy(Response::STATUS_OK, 'api/channel/1', json_encode($this->channelData));
+
+        $repo = new SubclassedChannelRepository($this->_client);
+        $channel = $repo->get($this->channelData['object']['id']);
+        $this->assertNotNull($channel);
+        $this->assertFalse($channel->isNew());
+        $this->assertInstanceOf('Mr\Api\Model\Channel', $channel);
+
+        $this->_clientMockAdapter->addResponseBy(Response::STATUS_OK, 'api/channel/1', '{"status": "ok"}');
+        $this->assertFalse($channel->wasBeforeDeleteCalled());
+        $this->assertFalse($channel->wasAfterDeleteCalled());
+        $channel->delete();
+        $this->assertTrue($channel->wasBeforeDeleteCalled());
+        $this->assertTrue($channel->wasAfterDeleteCalled());
+        $this->assertTrue($channel->isNew());
+        $this->assertFalse($channel->isModified());
+    }
+
+    public function testSaveOneChannelAndBeforeAfterHooksAreCalled() {
+        // Creating response with specific url
+        $this->_clientMockAdapter->addResponseBy(Response::STATUS_OK, 'api/channel/1', json_encode($this->channelData));
+
+        $repo = new SubclassedChannelRepository($this->_client);
+        $channel = $repo->get($this->channelData['object']['id']);
+        $this->assertNotNull($channel);
+        $this->assertFalse($channel->isNew());
+        $this->assertInstanceOf('Mr\Api\Model\Channel', $channel);
+
+        $this->_clientMockAdapter->addResponseBy(Response::STATUS_OK, 'api/channel/1', json_encode($this->channelData));
+        $this->assertFalse($channel->wasBeforeSaveCalled());
+        $this->assertFalse($channel->wasAfterSaveCalled());
+        $channel->save();
+        $this->assertTrue($channel->wasBeforeSaveCalled());
+        $this->assertTrue($channel->wasAfterSaveCalled());
+
+        $this->assertFalse($channel->isNew());
+        $this->assertFalse($channel->isModified());
+
+        $this->_clientMockAdapter->addResponseBy(Response::STATUS_OK, 'api/channel/1', json_encode($this->channelData));
+        $channel = $repo->create(array('name'=> 'some_name'));
+
+        $this->assertTrue($channel->isNew());
+        $this->assertFalse($channel->isModified());
+
+        $this->assertFalse($channel->wasBeforeSaveCalled());
+        $this->assertFalse($channel->wasAfterSaveCalled());
+        $channel->save();
+        $this->assertTrue($channel->wasBeforeSaveCalled());
+        $this->assertTrue($channel->wasAfterSaveCalled());
+
+        $this->assertFalse($channel->isNew());
+        $this->assertFalse($channel->isModified());
     }
 }
