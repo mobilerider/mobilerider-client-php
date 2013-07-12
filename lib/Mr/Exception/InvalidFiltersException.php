59f1ccd69a828febc7e553a48aaa9c4ff4b4bd56
Michel Perez Vargas <michel.perez8402@gmail.com>
2013-07-12 10:45:16 -0400

    Adds exception classes for errors invalid filters and invalid format.

---
+++ lib/Mr/Exception/InvalidFiltersException.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Mr\Exception;
+
+class InvalidFiltersException extends MrException
+{
+    public function __construct(array $invalidKeys, array $allowdKeys)
+    {
+    	$message = 'Some of the supplied filters are invalid. Valid filters are: ' . implode(', ', $allowdKeys);
+    	$message .= ' You supplied: ' . implode(', ', $invalidKeys);
+
+        parent::__construct($message);
+    }
+}
