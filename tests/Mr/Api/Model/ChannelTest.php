5f8e84dfb8a2fb66a57fdcb3c5feb605137c088e
Armando Pérez Marqués <armando.perez@mobilerider.com>
2013-07-18 11:32:19 -0400

    Added some tests

--- tests/Mr/Api/Model/ChannelTest.php
+++ tests/Mr/Api/Model/ChannelTest.php
@@ -87,4 +87,118 @@ class ChannelTest extends \PHPUnit_Framework_TestCase {
         $this->assertFalse($channel->isModified());
         $channel->save();
     }
+
+    /**
+     * @expectedException Mr\Exception\InvalidFormatException
+     */
+    public function testSetDataException() {
+        $channel = $this->getDummyChannel();
+        $channel->setData('some data?');
+    }
+
+    /**
+     * @expectedException Mr\Exception\InvalidFormatException
+     */
+    public function testUpdateDataException() {
+        $channel = $this->getDummyChannel();
+        $channel->updateData('some data?');
+    }
+
+    public function testSetData() {
+        $channel = $this->getDummyChannel();
+
+        $channel->{$channel->getKeyField()} = 1;
+        $channel->some_key = 'some_key_value';
+        $this->assertEquals('some_key_value', $channel->some_key);
+        $this->assertEquals('id', $channel->getKeyField());
+        $this->assertEquals(1, $channel->id);
+
+        $this->assertTrue($channel->isModified());
+        $channel->saved();
+        $this->assertFalse($channel->isModified());
+
+        $channel->setData(array(
+            'id' => 2,
+            'some_key' => 'other_key_value_from_array',
+            'some_other_key' => 'yet_another_key_value_from_array'
+        ));
+
+        $this->assertTrue($channel->isModified());
+        $this->assertEquals(2, $channel->{$channel->getKeyField()});
+        $this->assertEquals('other_key_value_from_array', $channel->some_key);
+        $this->assertEquals('yet_another_key_value_from_array', $channel->some_other_key);
+
+        $whatever = new \stdClass();
+        $whatever->id = 3;
+        $whatever->some_key = 'other_key_value_from_object';
+        $whatever->some_other_key = 'yet_another_key_value_from_object';
+
+        $channel->setData($whatever);
+
+        $this->assertTrue($channel->isModified());
+        $this->assertEquals(3, $channel->{$channel->getKeyField()});
+        $this->assertEquals('other_key_value_from_object', $channel->some_key);
+        $this->assertEquals('yet_another_key_value_from_object', $channel->some_other_key);
+
+        $apiobj_whatever = $this->getDummyChannel(array(
+            'id' => 4,
+            'some_key' => 'other_key_value_from_apiobject',
+            'some_other_key' => 'yet_another_key_value_from_apiobject'
+        ));
+        $channel->setData($apiobj_whatever);
+
+        $this->assertTrue($channel->isModified());
+        $this->assertEquals(4, $channel->{$channel->getKeyField()});
+        $this->assertEquals('other_key_value_from_apiobject', $channel->some_key);
+        $this->assertEquals('yet_another_key_value_from_apiobject', $channel->some_other_key);
+    }
+
+    public function testUpdateData() {
+        $channel = $this->getDummyChannel();
+
+        $channel->{$channel->getKeyField()} = 1;
+        $channel->some_key = 'some_key_value';
+        $this->assertEquals('some_key_value', $channel->some_key);
+        $this->assertEquals('id', $channel->getKeyField());
+        $this->assertEquals(1, $channel->id);
+
+        $this->assertTrue($channel->isModified());
+        $channel->saved();
+        $this->assertFalse($channel->isModified());
+
+        $channel->updateData(array(
+            'id' => 2,
+            'some_key' => 'other_key_value_from_array',
+            'some_other_key' => 'yet_another_key_value_from_array'
+        ));
+
+        $this->assertTrue($channel->isModified());
+        $this->assertEquals(1, $channel->{$channel->getKeyField()});
+        $this->assertEquals('other_key_value_from_array', $channel->some_key);
+        $this->assertEquals('yet_another_key_value_from_array', $channel->some_other_key);
+
+        $whatever = new \stdClass();
+        $whatever->id = 3;
+        $whatever->some_key = 'other_key_value_from_object';
+        $whatever->some_other_key = 'yet_another_key_value_from_object';
+
+        $channel->updateData($whatever);
+
+        $this->assertTrue($channel->isModified());
+        $this->assertEquals(1, $channel->{$channel->getKeyField()});
+        $this->assertEquals('other_key_value_from_object', $channel->some_key);
+        $this->assertEquals('yet_another_key_value_from_object', $channel->some_other_key);
+
+        $apiobj_whatever = $this->getDummyChannel(array(
+            'id' => 4,
+            'some_key' => 'other_key_value_from_apiobject',
+            'some_other_key' => 'yet_another_key_value_from_apiobject'
+        ));
+        $channel->updateData($apiobj_whatever);
+
+        $this->assertTrue($channel->isModified());
+        $this->assertEquals(1, $channel->{$channel->getKeyField()});
+        $this->assertEquals('other_key_value_from_apiobject', $channel->some_key);
+        $this->assertEquals('yet_another_key_value_from_apiobject', $channel->some_other_key);
+    }
 }
--- tests/Mr/Api/Model/MediaTest.php
+++ tests/Mr/Api/Model/MediaTest.php
@@ -87,4 +87,118 @@ class MediaTest extends \PHPUnit_Framework_TestCase {
         $this->assertFalse($media->isModified());
         $media->save();
     }
+
+    /**
+     * @expectedException Mr\Exception\InvalidFormatException
+     */
+    public function testSetDataException() {
+        $media = $this->getDummyMedia();
+        $media->setData('some data?');
+    }
+
+    /**
+     * @expectedException Mr\Exception\InvalidFormatException
+     */
+    public function testUpdateDataException() {
+        $media = $this->getDummyMedia();
+        $media->updateData('some data?');
+    }
+
+    public function testSetData() {
+        $media = $this->getDummyMedia();
+
+        $media->{$media->getKeyField()} = 1;
+        $media->some_key = 'some_key_value';
+        $this->assertEquals('some_key_value', $media->some_key);
+        $this->assertEquals('id', $media->getKeyField());
+        $this->assertEquals(1, $media->id);
+
+        $this->assertTrue($media->isModified());
+        $media->saved();
+        $this->assertFalse($media->isModified());
+
+        $media->setData(array(
+            'id' => 2,
+            'some_key' => 'other_key_value_from_array',
+            'some_other_key' => 'yet_another_key_value_from_array'
+        ));
+
+        $this->assertTrue($media->isModified());
+        $this->assertEquals(2, $media->{$media->getKeyField()});
+        $this->assertEquals('other_key_value_from_array', $media->some_key);
+        $this->assertEquals('yet_another_key_value_from_array', $media->some_other_key);
+
+        $whatever = new \stdClass();
+        $whatever->id = 3;
+        $whatever->some_key = 'other_key_value_from_object';
+        $whatever->some_other_key = 'yet_another_key_value_from_object';
+
+        $media->setData($whatever);
+
+        $this->assertTrue($media->isModified());
+        $this->assertEquals(3, $media->{$media->getKeyField()});
+        $this->assertEquals('other_key_value_from_object', $media->some_key);
+        $this->assertEquals('yet_another_key_value_from_object', $media->some_other_key);
+
+        $apiobj_whatever = $this->getDummyMedia(array(
+            'id' => 4,
+            'some_key' => 'other_key_value_from_apiobject',
+            'some_other_key' => 'yet_another_key_value_from_apiobject'
+        ));
+        $media->setData($apiobj_whatever);
+
+        $this->assertTrue($media->isModified());
+        $this->assertEquals(4, $media->{$media->getKeyField()});
+        $this->assertEquals('other_key_value_from_apiobject', $media->some_key);
+        $this->assertEquals('yet_another_key_value_from_apiobject', $media->some_other_key);
+    }
+
+    public function testUpdateData() {
+        $media = $this->getDummyMedia();
+
+        $media->{$media->getKeyField()} = 1;
+        $media->some_key = 'some_key_value';
+        $this->assertEquals('some_key_value', $media->some_key);
+        $this->assertEquals('id', $media->getKeyField());
+        $this->assertEquals(1, $media->id);
+
+        $this->assertTrue($media->isModified());
+        $media->saved();
+        $this->assertFalse($media->isModified());
+
+        $media->updateData(array(
+            'id' => 2,
+            'some_key' => 'other_key_value_from_array',
+            'some_other_key' => 'yet_another_key_value_from_array'
+        ));
+
+        $this->assertTrue($media->isModified());
+        $this->assertEquals(1, $media->{$media->getKeyField()});
+        $this->assertEquals('other_key_value_from_array', $media->some_key);
+        $this->assertEquals('yet_another_key_value_from_array', $media->some_other_key);
+
+        $whatever = new \stdClass();
+        $whatever->id = 3;
+        $whatever->some_key = 'other_key_value_from_object';
+        $whatever->some_other_key = 'yet_another_key_value_from_object';
+
+        $media->updateData($whatever);
+
+        $this->assertTrue($media->isModified());
+        $this->assertEquals(1, $media->{$media->getKeyField()});
+        $this->assertEquals('other_key_value_from_object', $media->some_key);
+        $this->assertEquals('yet_another_key_value_from_object', $media->some_other_key);
+
+        $apiobj_whatever = $this->getDummyMedia(array(
+            'id' => 4,
+            'some_key' => 'other_key_value_from_apiobject',
+            'some_other_key' => 'yet_another_key_value_from_apiobject'
+        ));
+        $media->updateData($apiobj_whatever);
+
+        $this->assertTrue($media->isModified());
+        $this->assertEquals(1, $media->{$media->getKeyField()});
+        $this->assertEquals('other_key_value_from_apiobject', $media->some_key);
+        $this->assertEquals('yet_another_key_value_from_apiobject', $media->some_other_key);
+    }
 }
