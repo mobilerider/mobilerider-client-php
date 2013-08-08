f4ce815ad679c41c6a798576d75476e5abb23493
Michel Perez Vargas <michel.perez8402@gmail.com>
2013-08-08 14:28:06 -0400

    Completes validator tests regarding int and array types and ip modifiers.

--- tests/Mr/Api/Util/ValidatorTest.php
+++ tests/Mr/Api/Util/ValidatorTest.php
@@ -21,6 +21,47 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
 		)
 	);
 
+	protected $typeInteger= array(
+		Validator::TYPES => array(Validator::TYPE_INT)
+	);
+
+	protected $typePositiveInteger = array(
+		Validator::TYPES => array(
+			Validator::TYPE => Validator::TYPE_INT,
+			Validator::MODIFIERS => array(Validator::MODIFIER_POSITIVE)
+		)
+	);
+
+	protected $typeNegativeInteger = array(
+		Validator::TYPES => array(
+			Validator::TYPE => Validator::TYPE_INT,
+			Validator::MODIFIERS => array(Validator::MODIFIER_NEGATIVE)
+		)
+	);
+
+	protected $typeArray = array(
+		Validator::TYPES => array(Validator::TYPE_ARRAY)
+	);
+
+	protected $typeArrayOfPositiveInts = array(
+		Validator::TYPES => array(
+			Validator::TYPE => Validator::TYPE_ARRAY,
+			Validator::MODIFIERS => array(
+				Validator::MODIFIER => Validator::MODIFIER_NESTED,
+				Validator::MODIFIER_VALIDATORS => array(
+					Validator::TYPES => array(
+						Validator::TYPE => Validator::TYPE_INT,
+						Validator::MODIFIERS => array(Validator::MODIFIER_POSITIVE)
+					)
+				)
+			)
+		)
+	);
+
+	protected $modifierIp = array(
+		Validator::MODIFIERS => array(Validator::MODIFIER_IP)
+	);
+
 	public function testConstraints()
 	{
 		$value = null;
@@ -40,5 +81,69 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
 
 		$value = array('not empty');
 		$this->assertTrue(Validator::validate($value, $this->requiredNestedValidator));
+	}
+
+	public function testNativeTypes()
+	{
+		// INTEGER
+
+		$value = 'not an integer';
+		$this->assertFalse(Validator::validate($value, $this->typeInteger));
+
+		$value = 1.5;
+		$this->assertFalse(Validator::validate($value, $this->typeInteger));
+
+		$value = 1;
+		$this->assertTrue(Validator::validate($value, $this->typeInteger));
+
+		$value = -1;
+		$this->assertFalse(Validator::validate($value, $this->typePositiveInteger));
+
+		$value = 1;
+		$this->assertTrue(Validator::validate($value, $this->typePositiveInteger));
+
+		$value = 1;
+		$this->assertFalse(Validator::validate($value, $this->typeNegativeInteger));
+
+		$value = -1;
+		$this->assertTrue(Validator::validate($value, $this->typeNegativeInteger));
+
+
+		// ARRAY
+
+		$value = 'not an array';
+		$this->assertFalse(Validator::validate($value, $this->typeArray));
+
+		$value = array();
+		$this->assertTrue(Validator::validate($value, $this->typeArray));
+
+		$value = 'not an array';
+		$this->assertFalse(Validator::validate($value, $this->typeArray));
+
+		$value = array();
+		$this->assertTrue(Validator::validate($value, $this->typeArray));
+
+
+		// ARRAY OF INTS
+
+		$value = array('not an integer');
+		$this->assertFalse(Validator::validate($value, $this->typeArrayOfPositiveInts));
+
+		$value = array(1);
+		$this->assertTrue(Validator::validate($value, $this->typeArrayOfPositiveInts));
+	}
+
+	public function testDataModifiers()
+	{
+		// IP
+
+		$value = 'not an ip';
+		$this->assertFalse(Validator::validate($value, $this->modifierIp));
+
+		$value = '127.0000'; // Mallformed ip
+		$this->assertFalse(Validator::validate($value, $this->modifierIp));
+
+		$value = '127.0.0.1';
+		$this->assertTrue(Validator::validate($value, $this->modifierIp));
 	}
 }
